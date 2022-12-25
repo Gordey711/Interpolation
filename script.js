7ff5e6c288901870d7a6210f049f19061ecb6b69
Gordey <gordeylog71@gmail.com>
2022-12-25 16:02:46 +0300

    Interpolation

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const category = 'toys';
+
+// console.log('https://someurl.com/' + category + '/' + '4');
+console.log(`https://someurl.com/${category}/5`);  // Только когда косые ковычки
+
+const user = 'Ivan';
+
+alert(`Hello!, ${user}`);
